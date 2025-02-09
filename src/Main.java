28df5c8a8b42894209e2b6c1ff2dac9a27cd3eea
Gabriel Mejia Moreno <gm2209550@gmail.com>
2025-02-09 17:01:00 -0500

    Created a small luck-based game.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+import java.util.Random;
+
 public class Main {
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+        int counter = 0;
+        boolean stillIn = true;
+
+        System.out.println("Hello and welcome!");
+
+        System.out.println("Let's test your luck! Input any number between 1 and 10");
+
+        while (stillIn == true) {
+            int i = scan.nextInt();
+            if (i == rand.nextInt(11)) {
+                counter++;
+                System.out.println("Again!");
+            }
+            else {
+                stillIn = false;
+                scan.close();
+                System.out.println("You lost!");
+            }
+        }
+        System.out.println("You got a score of " + counter + "!");
     }
 }
